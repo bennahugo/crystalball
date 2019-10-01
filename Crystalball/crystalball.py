d1ac93497335b1da3b1dd040e26e075d4774b03f
Benna Hugo <bennahugo@aol.com>
2019-10-01 11:55:19 +0200

    ok

--- Crystalball/crystalball.py
+++ Crystalball/crystalball.py
@@ -192,8 +192,9 @@ def predict(args):
     spw_ds = tables["SPECTRAL_WINDOW"]
     pol_ds = tables["POLARIZATION"]
     frequencies = np.sort([spw_ds[ddid].CHAN_FREQ.data.flatten().values 
-                            for ddid in ddid_ds[dd].SPECTRAL_WINDOW_ID 
-                            for dd in range(len(ddid_ds))])
+                            for dd in range(len(ddid_ds))
+                            for ddid in ddid_ds[dd].SPECTRAL_WINDOW_ID])
+                            
     
     # cluster sources and refit. This only works for delta scale sources
     def __cluster(comp_type, radec, stokes, spec_coeff, ref_freq, log_spec_ind,
